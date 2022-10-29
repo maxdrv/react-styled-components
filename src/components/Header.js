df66f9fcae26dd7ff0f776ada284e0e053be8ae6
maxdrv <maxdrv.stud@gmail.com>
2022-10-29 10:03:11 +0500

    basics of styled components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyledHeader} from "./styled/Header.styled";
+
+const Header = (props) => (
+    <StyledHeader bg='red'>
+        <h1>Hubble</h1>
+    </StyledHeader>
+);
+
+export default Header;
