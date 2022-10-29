7e713b77e8e94b6d63a101851c68e613b414f0dc
maxdrv <maxdrv.stud@gmail.com>
2022-10-29 10:35:31 +0500

    added public images and styled Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {StyledHeader} from "./styled/Header.styled";
+import {Logo, Nav, StyledHeader} from "./styled/Header.styled";
+import {Container} from "./styled/Container.styled";
+import {Button} from "./styled/Button.styled";
 
 const Header = (props) => (
     <StyledHeader>
-        <h1>Hubble</h1>
+        <Container>
+            <Nav>
+                <Logo src='./images/logo.svg' alt=''/>
+                <Button>Try It Free</Button>
+            </Nav>
+        </Container>
     </StyledHeader>
 );
 
