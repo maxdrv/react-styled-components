9af0c29ee0424c484ab7141840f61d9d1269d699
maxdrv <maxdrv.stud@gmail.com>
2022-10-29 10:56:09 +0500

    applied flex for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Logo, Nav, StyledHeader} from "./styled/Header.styled";
+import {Image, Logo, Nav, StyledHeader} from "./styled/Header.styled";
 import {Container} from "./styled/Container.styled";
 import {Button} from "./styled/Button.styled";
+import {Flex} from "./styled/Flex";
 
 const Header = (props) => (
     <StyledHeader>
@@ -10,6 +11,20 @@ const Header = (props) => (
                 <Logo src='./images/logo.svg' alt=''/>
                 <Button>Try It Free</Button>
             </Nav>
+            <Flex>
+                <div>
+                    <h1>Build The Community Your Fans Will Love</h1>
+                    <p>
+                        Huddle re-imagines the way we build communities. You have a voice,
+                        but so does your audience. Create connections with your users as
+                        you engage in genuine discussion.
+                    </p>
+                    <Button bg='#ff0099' color='#fff'>
+                        Get Started For Free
+                    </Button>
+                </div>
+                <Image src='./images/illustration-mockups.svg' alt=''/>
+            </Flex>
         </Container>
     </StyledHeader>
 );
